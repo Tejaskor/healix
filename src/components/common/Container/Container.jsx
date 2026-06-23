12c5b671268b342566eb4ebe613170fdedeac964
Mayur@6004 <mayur.jadhavytc@gmail.com>
2026-06-23 15:23:50 +0530

    Updates

--- src/components/common/Container/Container.jsx
+++
@@ -1,9 +0,0 @@
-import './Container.scss'
-
-const Container = ({ children, className = '', narrow = false }) => {
-  const classes = `container ${narrow ? 'container--narrow' : ''} ${className}`.trim()
-
-  return <div className={classes}>{children}</div>
-}
-
-export default Container
--- src/pages/AssessmentPage.jsx
+++
@@ -1,8 +0,0 @@
-import AssessmentContainer from '@/components/assessment/AssessmentContainer'
-import assessmentConfig from '@/config/assessmentConfig'
-
-const AssessmentPage = () => {
-  return <AssessmentContainer config={assessmentConfig} />
-}
-
-export default AssessmentPage
